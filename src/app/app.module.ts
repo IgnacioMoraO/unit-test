e16dfbab2b2e19000b9b2ebaf04fd56b351f2a9a
Daniel Castro <daniel.castro@basedos.cl>
2020-11-12 11:58:32 -0300

    Implementacion de componente incrementador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './components/test/test.component';
+import { IncrementadorComponent } from './components/incrementador/incrementador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    IncrementadorComponent
   ],
   imports: [
     BrowserModule,
